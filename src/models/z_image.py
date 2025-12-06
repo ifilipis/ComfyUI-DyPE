e9da8e3fc8b49bff9d98f1af3b2e21e75e45a1a0
ifilipis <40601736+ifilipis@users.noreply.github.com>
2025-12-06 07:42:47 +0200

    Stop scaling Z-Image positions in DyPE embedding

--- src/models/z_image.py
+++ src/models/z_image.py
@@ -12,18 +12,6 @@ class PosEmbedZImage(DyPEBasePosEmbed):
         grid_spans = [self._axis_range(pos_full[..., i]) for i in range(pos_full.shape[-1])]
 
         pos = pos_full
-        if self.base_hw is not None and pos.shape[-1] >= 3:
-            pos = pos_full.clone()
-            h_tokens = grid_spans[1] if len(grid_spans) > 1 else 0.0
-            w_tokens = grid_spans[2] if len(grid_spans) > 2 else 0.0
-
-            if self.base_hw[0] > 0:
-                h_scale = h_tokens / float(self.base_hw[0])
-                pos[..., 1] = pos[..., 1] * h_scale
-
-            if self.base_hw[1] > 0:
-                w_scale = w_tokens / float(self.base_hw[1])
-                pos[..., 2] = pos[..., 2] * w_scale
         freqs_dtype = torch.bfloat16 if pos.device.type == 'cuda' else torch.float32
         components = self.get_components(pos, freqs_dtype, grid_spans, base_pos=pos_full)
 
